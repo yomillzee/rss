a4465158f4da79c4a7559066e6841a68c483d7aa
yomillzee <mjmillss@gmail.com>
2026-06-14 07:42:13 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,27 @@
-function stripHtml(html){
-
-    const div = document.createElement("div");
-    div.innerHTML = html;
-
-    return div.textContent ||
-           div.innerText ||
-           "";
-
-}
-
 function formatDate(dateString){
 
-    return "DATE TEST";
+    if(!dateString) return "";
+
+    const datePart =
+        dateString.split(" ")[0];
+
+    const [year,month,day] =
+        datePart.split("-");
+
+    const date =
+        new Date(
+            Number(year),
+            Number(month)-1,
+            Number(day)
+        );
+
+    return date.toLocaleDateString(
+        undefined,
+        {
+            month:"short",
+            day:"numeric",
+            year:"numeric"
+        }
+    );
 
 }
