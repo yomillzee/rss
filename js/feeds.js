6839df4d27b5675488a728aba3a7ec1c0e43a2bf
yomillzee <mjmillss@gmail.com>
2026-06-14 07:28:05 -0400

    Create js/feeds.js

---
+++ js/feeds.js
@@ -0,0 +1,45 @@
+const feeds = [
+
+{
+    name:"OpenAI",
+    category:"AI",
+    url:"https://openai.com/news/rss.xml"
+},
+
+{
+    name:"Search Engine Land",
+    category:"Search",
+    url:"https://searchengineland.com/feed"
+},
+
+{
+    name:"Ahrefs Blog",
+    category:"Search",
+    url:"https://ahrefs.com/blog/feed"
+},
+
+{
+    name:"HubSpot Marketing",
+    category:"Marketing",
+    url:"https://blog.hubspot.com/marketing/rss.xml"
+},
+
+{
+    name:"Associated Press Politics",
+    category:"Politics",
+    url:"https://apnews.com/hub/politics/rss"
+},
+
+{
+    name:"NPR Politics",
+    category:"Politics",
+    url:"https://feeds.npr.org/1014/rss.xml"
+},
+
+{
+    name:"Financial Times",
+    category:"Business",
+    url:"https://www.ft.com/rss/home"
+}
+
+];
