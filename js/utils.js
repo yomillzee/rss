cd907165deb6d7818bf2bf009e71afb8a29b2609
yomillzee <mjmillss@gmail.com>
2026-06-14 07:34:44 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,6 @@
 function stripHtml(html){
 
-    const div =
-        document.createElement("div");
-
+    const div = document.createElement("div");
     div.innerHTML = html;
 
     return div.textContent ||
@@ -13,47 +11,6 @@ function stripHtml(html){
 
 function formatDate(dateString){
 
-    if(!dateString) return "";
-
-    try{
-
-        let date;
-
-        if(dateString.includes(" ")){
-
-            const [datePart] =
-                dateString.split(" ");
-
-            const [year,month,day] =
-                datePart.split("-");
-
-            date = new Date(
-                Number(year),
-                Number(month)-1,
-                Number(day)
-            );
-
-        } else {
-
-            date = new Date(dateString);
-
-        }
-
-        return date.toLocaleDateString(
-            undefined,
-            {
-                month:"short",
-                day:"numeric",
-                year:"numeric"
-            }
-        );
-
-    }
-
-    catch{
-
-        return dateString;
-
-    }
+    return "DATE TEST";
 
 }
